61f45c60913a8e022690d5d50d7cd81c252198a4
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2022-03-30 12:28:44 +0100

    Not much

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -6,9 +6,32 @@ public class ExampleTest {
 
     @Test
     public void testExpression() {
-        TestUtils.parse("2+3\n10+20\n");
-        TestUtils.parse("import io;\n");
-        System.out.println("ASDSADADAS");
+
+        // Import Tests
+        TestUtils.parse("import io;\n", "ImportDeclaration");
+        TestUtils.parse("import org.junit.Test;\n");
+        TestUtils.parse("import io; import org.junit.Test;");
+
+        // Type Tests
+        TestUtils.parse("int alo\n");
+        TestUtils.parse("int[] alo\n");
+        TestUtils.parse("boolean alo\n");
+
+        // Expression Tests
+        TestUtils.parse("a + b");
+        TestUtils.parse("1 < a");
+        TestUtils.parse("alo && True");
+        TestUtils.parse("vez * False");
+        TestUtils.parse("123.length");
+        TestUtils.parse("False.append(1 + a)");
+        TestUtils.parse("new 123()");
+        TestUtils.parse("new 123[True.length]");
+
+        System.out.println("Resto de um bom dia!");
+
+        var parserResult = TestUtils.parse("import io;\n");
+        TestUtils.noErrors(parserResult.getReports());
+
         // var parserResult = TestUtils.parse("2+3\n10+20\n");
         // parserResult.getReports().get(0).getException().get().printStackTrace();
         // // System.out.println();
